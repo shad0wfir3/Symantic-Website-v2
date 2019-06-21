ec8836b0d415295cb3046e26065fdc0a07f7c0ca
Eamon Boonzaaier <eamon@symanticreative.com>
2019-06-21 22:54:31 +0200

    updated stuff

--- resources/views/theme/pages/services/graphic_design.blade.php
+++ resources/views/theme/pages/services/graphic_design.blade.php
@@ -126,28 +126,59 @@
         </div>
     </div>
 
-    <!-- Parallax Block
-  ================================================== -->
-
-    <div class="section padding-top-bottom over-hide">
-        <div class="parallax-1" style="background-image: url({{ asset('img/services/graphic-design/header.jpg') }})"></div>
+    <!-- Skills Parallax Block
+      ================================================== -->
+    <div class="section padding-top-small padding-bottom-1st over-hide">
+        <div class="parallax-1" style="background-image: url({{ asset('img/services/graphic-design/cta_1.jpg') }})"></div>
         <div class="grey-fade-over"></div>
         <div class="container z-bigger">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="main-title on-dark text-center">
-                        <div class="main-subtitle-top mb-4">time to get started</div>
-                        <h3>Want to work with us?</h3>
-                        <div class="main-subtitle-bottom mt-3">Expert professional design services at affordable prices.</div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="main-title on-dark text-left">
+                        <div class="main-subtitle-top mb-4">Graphic Design</div>
+                        <h4>Printed, Digital, Social, UX.<br> Graphic design for any project.</h4>
+                        <div class="main-subtitle-bottom smaller mt-3">a unique approach to design.</div>
+                    </div>
+                </div>
+                <div class="col-lg-6 mt-5 mt-lg-4" id="progress">
+                    <div class="progress-container">
+                        <span class="progress-badge dark">Research</span>
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="94" aria-valuemin="0" aria-valuemax="100" style="width: 87%;">
+                                <span class="progress-value"><span class="counter">94</span>%</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="progress-container mt-4">
+                        <span class="progress-badge dark">Prototype</span>
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
+                                <span class="progress-value"><span class="counter">100</span>%</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="progress-container mt-4">
+                        <span class="progress-badge dark">Design</span>
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 93%;">
+                                <span class="progress-value"><span class="counter">100</span>%</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="progress-container mt-4">
+                        <span class="progress-badge dark">Release</span>
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="98" aria-valuemin="0" aria-valuemax="100" style="width: 93%;">
+                                <span class="progress-value"><span class="counter">98</span>%</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
     <!-- Call To Action Block
     ================================================== -->
-
     <div class="section background-white z-bigger-2">
         <div class="container">
             <div class="row justify-content-center">
@@ -155,14 +186,13 @@
                     <div class="call-box-3 p-5 dark text-center background-dark rounded-2 drop-shadow">
                         <i class="funky-ui-icon icon-Digital-Drawing"></i>
                         <h5 class="mt-4 mb-3 color-white">Request a quote!</h5>
-                        <p class="mb-5">Interrested in a new logo, business card, brochure or flyers? Request a quote below or contact us directly!</p>
+                        <p class="mb-5">Interested in a rebrand, new logo, business card or online grapics? Look no further, request a quote below.</p>
                         <a href="{{route('theme.pages.request_quote')}}" class="btn btn-primary btn-simple btn-round btn-long" >request a quote</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    @include('theme.includes.seperator-line')
 
 @endsection
 @push('scripts')
